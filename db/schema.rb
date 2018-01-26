090164923b2a77c183c79dc1a7ff5591daceeec0
Travis Petersen <travis.e.petersen@gmail.com>
2018-01-26 01:59:33 +0000

    migrate birds

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180126015313) do
+ActiveRecord::Schema.define(version: 20180126015806) do
+
+  create_table "birds", force: :cascade do |t|
+    t.string   "name"
+    t.string   "family"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "sightings", force: :cascade do |t|
     t.string   "date"
