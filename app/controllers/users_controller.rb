e227cc4912589d0724a6c9acce21387b05e35dc8
Travis Petersen <travis.e.petersen@gmail.com>
2018-01-26 23:32:51 +0000

    build signup post request

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,5 +8,17 @@ class UsersController < ApplicationController
     end
   end
   
+  post '/users' do 
+    user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+    
+    if user.save
+      session[:user_id] = user.id
+      redirect "/users/#{user.slug}"
+      
+    else
+      redirect "/users/signup"
+    end
+  end
+  
   
 end
