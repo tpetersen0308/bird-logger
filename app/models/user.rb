1b4d8f7cb04fcf36015bc85be8fcf4dc63915a41
Travis Petersen <travis.e.petersen@gmail.com>
2018-01-26 01:47:39 +0000

    add user macros

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  
+  has_many :sightings
+  has_many :birds, through: :sightings
 end
