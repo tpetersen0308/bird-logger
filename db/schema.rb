e9ab7baeede82813b7221eef44a1a7418a99ceed
Travis Petersen <travis.e.petersen@gmail.com>
2018-01-26 01:54:30 +0000

    migrate sightings

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180126013337) do
+ActiveRecord::Schema.define(version: 20180126015313) do
+
+  create_table "sightings", force: :cascade do |t|
+    t.string   "date"
+    t.string   "time"
+    t.string   "location"
+    t.integer  "user_id"
+    t.integer  "bird_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
