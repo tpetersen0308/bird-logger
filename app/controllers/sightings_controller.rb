6c9e38a5a754603e60ad41a070b7e85736d67b43
Travis Petersen <travis.e.petersen@gmail.com>
2018-01-27 02:35:45 +0000

    build post request for new sightings

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -18,4 +18,19 @@ class SightingsController < ApplicationController
       redirect '/'
     end
   end
+  
+  post '/sightings' do
+    sighting = Sighting.new(:date => params[:date], :time => params[:time], :location => params[:location], :user_id => current_user.id, :bird_id => params[:bird_id])
+    
+    if sighting.save
+      redirect "/sightings/#{sighting.id}"
+    elsif params[:bird][:name] != ""
+      sighting.bird = Bird.find_or_create_by(:name => params[:bird][:name])
+      sighting.save
+      redirect "/sightings/#{sighting.id}"
+    else
+      flash[:message] = "A sighting cannot be created without selecting or entering a bird."
+      redirect '/sightings/new'
+    end
+  end
 end
