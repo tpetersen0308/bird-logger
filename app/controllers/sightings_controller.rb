b011709d7125009346e1ad50329269a0b9db6ab2
Travis Petersen <travis.e.petersen@gmail.com>
2018-01-27 01:37:27 +0000

    add sightings index get request

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,37 +1,12 @@
 class SightingsController < ApplicationController
 
-  # GET: /sightings
-  get "/sightings" do
-    erb :"/sightings/index.html"
-  end
-
-  # GET: /sightings/new
-  get "/sightings/new" do
-    erb :"/sightings/new.html"
-  end
-
-  # POST: /sightings
-  post "/sightings" do
-    redirect "/sightings"
-  end
-
-  # GET: /sightings/5
-  get "/sightings/:id" do
-    erb :"/sightings/show.html"
-  end
-
-  # GET: /sightings/5/edit
-  get "/sightings/:id/edit" do
-    erb :"/sightings/edit.html"
-  end
-
-  # PATCH: /sightings/5
-  patch "/sightings/:id" do
-    redirect "/sightings/:id"
-  end
-
-  # DELETE: /sightings/5/delete
-  delete "/sightings/:id/delete" do
-    redirect "/sightings"
+  get '/sightings' do
+    if logged_in?
+      @sightings = Sighting.all
+      erb :'sightings/index'
+    else
+      flash[:message] = "You must be logged in to view sightings. Please log in or sign up below."
+      redirect '/'
+    end
   end
 end
