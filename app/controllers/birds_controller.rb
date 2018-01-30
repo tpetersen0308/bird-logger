87929d6ac1565a50375d4e587596fee84a566733
Travis Petersen <travis.e.petersen@gmail.com>
2018-01-30 00:12:08 +0000

    build birds get request

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,37 +1,12 @@
 class BirdsController < ApplicationController
 
-  # GET: /birds
   get "/birds" do
-    erb :"/birds/index.html"
-  end
-
-  # GET: /birds/new
-  get "/birds/new" do
-    erb :"/birds/new.html"
-  end
-
-  # POST: /birds
-  post "/birds" do
-    redirect "/birds"
-  end
-
-  # GET: /birds/5
-  get "/birds/:id" do
-    erb :"/birds/show.html"
-  end
-
-  # GET: /birds/5/edit
-  get "/birds/:id/edit" do
-    erb :"/birds/edit.html"
-  end
-
-  # PATCH: /birds/5
-  patch "/birds/:id" do
-    redirect "/birds/:id"
-  end
-
-  # DELETE: /birds/5/delete
-  delete "/birds/:id/delete" do
-    redirect "/birds"
+    if logged_in?
+      @birds = Bird.all.uniq
+      erb :'birds/index'
+    else
+      flash[:message] = "You must be logged in to view birds. Please log in or sign up below."
+      redirect '/'
+    end
   end
 end
