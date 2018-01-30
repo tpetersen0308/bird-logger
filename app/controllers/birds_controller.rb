2f5f1cd028c7b9113f9bf30968c32558c794b0bb
Travis Petersen <travis.e.petersen@gmail.com>
2018-01-30 00:39:01 +0000

    build bird show page

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -9,4 +9,14 @@ class BirdsController < ApplicationController
       redirect '/'
     end
   end
+  
+  get "/birds/:slug" do
+    if logged_in?
+      @bird = Bird.find_by_slug(params[:slug])
+      erb :"birds/show"
+    else
+      flash[:message] = "You must be logged in to view birds. Please log in or sign up below."
+      redirect '/'
+    end
+  end
 end
